31c7bbfaed36515262b2f7f59cb77abb8cb1f57e
Hans Oliveira <hansnery@gmail.com>
2021-11-29 16:59:00 -0300

    Create posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Posted successfully"
+      redirect_to posts_path
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:text)
+    end
+end
