65b90b6c985a5c403288fa3b30e07bb390c6d9dd
Hans Oliveira <hansnery@gmail.com>
2021-12-20 18:16:20 -0300

    New friendship request code

--- app/models/friend_request.rb
+++
@@ -1,4 +0,0 @@
-class FriendRequest < ApplicationRecord
-  belongs_to :requested, class_name: 'User', foreign_key: 'requested_id'
-  belongs_to :requester, class_name: 'User', foreign_key: 'requester_id'
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_170826) do
+ActiveRecord::Schema.define(version: 2021_12_20_203415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_12_15_170826) do
     t.bigint "requester_id"
     t.index ["requested_id"], name: "index_friend_requests_on_requested_id"
     t.index ["requester_id"], name: "index_friend_requests_on_requester_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "friend_id"
+    t.boolean "confirmed", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -54,4 +63,5 @@ ActiveRecord::Schema.define(version: 2021_12_15_170826) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendships", "users"
 end
