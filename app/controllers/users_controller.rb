5cf6ada7a0669cf1b3c7c597de28dd3e3e929751
Hans Oliveira <hansnery@gmail.com>
2022-02-22 17:12:56 -0300

    Add notification and accept friendship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,4 +24,19 @@ class UsersController < ApplicationController
     Friendship.where(user_id: current_user.id, friend_id: @user.id).destroy_all
     redirect_back fallback_location: root_path
   end
+
+  def accept_friendship_request
+    @user = User.all.find(params[:id])
+    @friendship = Friendship.where(user_id: @user.id, friend_id: current_user.id)
+    @friendship = @friendship[0]
+    @friendship.pending = false
+    @friendship.save
+    redirect_back fallback_location: root_path
+  end
+
+  def cancel_friendship_request
+    @user = User.all.find(params[:id])
+    Friendship.where(user_id: @user.id, friend_id: current_user.id).destroy_all
+    redirect_back fallback_location: root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   put 'posts/:id/dislike', to: 'posts#dislike', as: 'dislike'
   put 'users/:id/befriend', to: 'users#befriend', as: 'befriend'
   put 'users/:id/break_friendship', to: 'users#break_friendship', as: 'break_friendship'
+  put 'users/:id/accept_friendship_request', to: 'users#accept_friendship_request', as: 'accept_friendship_request'
+  put 'users/:id/cancel_friendship_request', to: 'users#cancel_friendship_request', as: 'cancel_friendship_request'
 end
