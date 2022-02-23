19398ea13ec5e479413c7d39b09943797fbd599a
Hans Oliveira <hansnery@gmail.com>
2022-02-23 14:46:33 -0300

    Add timeline

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,12 @@
 class HomeController < ApplicationController
+  def index
+    @post = Post.new
+    @friends = current_user.friends
+    @latest_posts = []
+    @friends.each do |friend|
+      friend.posts.map { |post| @latest_posts << post }
+    end
+    current_user.posts.map { |post| @latest_posts << post }
+    @latest_posts = @latest_posts.sort_by(&:created_at)
+  end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,5 @@
 class Friendship < ApplicationRecord
   belongs_to :user
-  belongs_to :friend, class_name: 'User', foreign_key: "user_id"
-  validates :user_id, uniqueness: { scope: :friend_id }
+  belongs_to :friend, class_name: 'User', foreign_key: "friend_id"
+  validates :friend_id, uniqueness: { scope: :user_id }
 end
